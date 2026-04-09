9e4cfe51f19a56f471b4180ff55c3fd85a75d397
phamhuynhbaohoan <phamhuynhbaohoan331@gmail.com>
2026-04-09 16:39:26 +0700

    feat(api): add avatarUrl field to User model - Add avatarUrl column to Users table via database migration - Update User model to include avatarUrl field

--- models/User.js
+++ models/User.js
@@ -56,6 +56,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: true,
       },
+      avatarUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {
       getterMethods: {
